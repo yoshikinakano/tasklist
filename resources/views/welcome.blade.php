80558aa5beb7ef203f2e0fa5212d35a7a818e803
yoshiki.nakano <nakano.yoshiki@a-tm.co.jp>
2018-05-15 06:58:17 +0000

    fix welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    @if ((\Auth::user()->id) == $user->id)
-        <div class="row">
-            <aside class="col-md-4">
-            </aside>
-            <div class="col-xs-8">
-                
-                @include('users.show', ['tasks' => $tasks, 'user' => $user])
-              
+    @if (\Auth::check())
+        @if ((\Auth::user()->id) == $user->id)
+            <div class="row">
+                <aside class="col-md-4">
+                </aside>
+                <div class="col-xs-8">
+                    
+                    @include('users.show', ['tasks' => $tasks, 'user' => $user])
+                  
+                </div>
+            </div>
+        @else
+           <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Microposts</h1>
             </div>
         </div>
+        @endif
     @else
         <div class="center jumbotron">
             <div class="text-center">
