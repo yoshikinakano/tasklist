1327208a6b892ea800f64a40a3ce7581007b1729
yoshiki.nakano <nakano.yoshiki@a-tm.co.jp>
2018-05-18 01:39:05 +0000

    fix design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,7 +4,7 @@
 
     <h1>タスク新規作成ページ</h1>
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
              {!! Form::model($task, ['route' => 'tasks.store']) !!}
             
                 <div class="form-group">
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,7 +4,7 @@
     
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('content', 'タスク内容:') !!}
